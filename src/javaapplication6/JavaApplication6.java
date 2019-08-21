ebea8c9423be5dc374aa1641da5d1f88d8d17a10
Estudiante <Estudiante@SSS-PC17>
2019-08-21 11:34:39 -0500

    2.0

--- src/javaapplication6/JavaApplication6.java
+++ src/javaapplication6/JavaApplication6.java
@@ -5,49 +5,62 @@
  */
 package javaapplication6;
 
+import java.util.Scanner;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Estudiante
- */
+
 public class JavaApplication6 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
+        ventana v1 = new ventana();
         
         
-        ventana nue = new ventana();
-        nue.show(true);
-        // clase
-         
+        
+        
+        
+        
+        
+        
+        
+        
+        
         /*
-        
-        int codigo,n;
-        double NotFin = 0;
-        student.setNombre(JOptionPane.showInputDialog("Ingrese el nombre del estudiante: "));
-        student.setAsignatura(JOptionPane.showInputDialog("Ingrese el nombre de la asignatura: "));
-        student.setN(Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de notas: ")));
-        
+        Scanner entrada = new Scanner(System.in);
+        Estudiante student = new Estudiante();
+        String nombre,asignatura;
+        int n;
+        double notFin = 0;
+        System.out.print("Ingrese el nombre del estudiante: ");
+        nombre = entrada.next();
+        student.setNombre(nombre);
+        System.out.print("Ingrese el nombre de la asignatura: ");
+        asignatura = entrada.next();
+        student.setAsignatura(asignatura);
+        System.out.println("Ingrese la cantidad de notas: ");
+        n = entrada.nextInt();
+        student.setN(n);
         double [] notas = new double[student.getN()];
         int [] porcentajes = new int[student.getN()];
         JOptionPane.showMessageDialog(null, "Por favor ingrese la nota desde 0 hasta 50 ");
         for (int i = 0; i < student.getN(); i++) {
                 notas[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la nota #"+ (i+1)+": " ));   
                 porcentajes[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el porcertanje de la nota #"+ (i+1)+": "));
-                NotFin += (notas[i]*porcentajes[i]/100);
+                notFin += (notas[i]*porcentajes[i]/100);
         }
-        if (NotFin < 30){
-            JOptionPane.showMessageDialog(null, "Su Nota final es de: " + NotFin + " \nLo sentimos "+ student.getNombre() +" pero no aprobo la materia de "+student.getAsigantura()+".", "Calificacion", JOptionPane.ERROR_MESSAGE);
+        if (notFin < 3 && notFin >= 2.6 ){
+            JOptionPane.showMessageDialog(null, "Su Nota final es de: " + notFin + " \nSr "+student.getNombre() +" puede habilitar la materia de "+student.getAsigantura()+".", "Calificacion", JOptionPane.ERROR_MESSAGE);
         }
-        else{
-        JOptionPane.showMessageDialog(null, "Su Nota final es de: " + NotFin + "\nFelicidades "+ student.getNombre()+" aprobo la materia de "+ student.getAsigantura()+".", "Calificacion", JOptionPane.INFORMATION_MESSAGE);
+        else if(notFin < 2.6){
+            JOptionPane.showMessageDialog(null, "Su Nota final es de: " + notFin + " \n Lo sentimos Sr "+ student.getNombre() +" reprobo la materia de "+student.getAsigantura()+".", "Calificacion", JOptionPane.ERROR_MESSAGE);
+        } 
+        else {
+        JOptionPane.showMessageDialog(null, "Su Nota final es de: " + notFin + "\nFelicidades "+ student.getNombre()+" aprobo la materia de "+ student.getAsigantura()+".", "Calificacion", JOptionPane.INFORMATION_MESSAGE);
         }
+*/
     }
-        
-    }*/
-    }
+       
 }
+    
+
